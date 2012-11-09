3157103c058c541b06970f74b6663c28e7a25c5f
Jeremy Seago <seagoj@gmail.com>
2012-11-09 10:39:44 -0600

    Initial commit

---
+++ tarCookbooks.sh
@@ -0,0 +1,18 @@
+dir='cookbooks'
+rm -R -f cookbooks.tar.gz
+mkdir $dir
+cd $dir
+
+git clone git://github.com/seagoj/cookbooks_php-fastcgi.git php-fastcgi
+git clone git://github.com/opscode-cookbooks/nginx.git
+git clone git://github.com/opscode-cookbooks/mysql.git
+git clone git://github.com/opscode-cookbooks/build-essential.git
+git clone git://github.com/opscode-cookbooks/ohai.git
+git clone git://github.com/opscode-cookbooks/apt.git
+git clone git://github.com/opscode-cookbooks/openssl.git
+
+cp -r ../cookbooks_mine/nginx/files/default/nginx.conf ../cookbooks/nginx/templates/default/default-site.erb 
+
+cd ..
+tar zcvf cookbooks.tar.gz $dir
+#rm -R -f cookbooks
