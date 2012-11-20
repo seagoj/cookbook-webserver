e2380015c262f772c52a5d55c2b656ec787f39bf
Jeremy Seago <seagoj@gmail.com>
2012-11-20 15:20:07 -0600

    updating cookbooks.tar.gz

---
+++ tarCookbooks.rb
@@ -0,0 +1,39 @@
+require 'net/ftp'
+
+#variables
+dir = 'cookbooks'
+method = 'git'
+
+ftp_site = ''
+ftp_user = ''
+ftp_pass = ''
+
+unless File.directory?(dir)
+	Dir.mkdir(dir)		
+end
+
+if File.exists?(dir+'.tar.gz')
+	File.delete(dir+'.tar.gz')
+end
+
+system('git clone git://github.com/seagoj/cookbook-apt.git '+dir+'/apt')
+system('git clone git://github.com/opscode-cookbooks/mysql.git '+dir+'/mysql')
+system('git clone git://github.com/seagoj/cookbook-nginx.git '+dir+'/nginx')
+system('git clone git://github.com/opscode-cookbooks/openssl.git '+dir+'/openssl')
+system('git clone git://github.com/seagoj/cookbook-redis.git '+dir+'/redis')
+system('git clone git://github.com/seagoj/cookbook-php5-fpm.git '+dir+'/php5-fpm')
+system('git clone git://github.com/seagoj/cookbook-bootstrap.git '+dir+'/bootstrap')
+system('tar zcvf '+dir+'.tar.gz '+dir)
+
+case method
+	when 'git'
+		system('git add '+dir+'.tar.gz')
+		system('git add tarCookbooks.rb')
+		system('git commit -m "updating '+dir+'.tar.gz"')
+		system('git push git@github.com:seagoj/cookbook.git master')
+		puts Dir.pwd
+	when 'ftp'
+		ftp = Net::FTP.new(ftp_site)
+		ftp.login(ftp_user,ftp_pass)
+		ftp.put(dir+'.tar.gz','downloads/'+dir+'.tar.gz')
+end
