cd441feb5c1f07a0ec50b11fc93197bd73c6f6bc
Jeremy Seago <seagoj@gmail.com>
2012-11-26 21:27:11 -0600

    Commit of default dotfiles

--- NewRepo.rb
+++ NewRepo.rb
@@ -1,5 +1,23 @@
 # NewRepo
+require 'fileutils'
+require 'grit'
+
+dotfilesPath = '../dotfiles'
+
+project = ARGV[0]
+puts project
+
+
+
+unless(File.exists?(dotfilesPath))
+	system('git clone git@github.com:seagoj/dotfiles.git '+dotfilesPath)
+else
+	system('cd '+dotfilesPath+' && git fetch')
+end
 
 system('git init')
-File.copy('../dotfiles/.gitattributes','.')
-File.copy('../dotfiles/.gitignore','.')
+system('git add *')
+system('git commit -m "Commit of default dotfiles')
+system('git remote add origin git@github.com:seagoj/project.git')
+FileUtils.cp(dotfilesPath+'/.gitattributes','.')
+FileUtils.cp(dotfilesPath+'/.gitignore','.')
